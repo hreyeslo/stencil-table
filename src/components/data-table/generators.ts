1d6a26d6002941d82d661fd3b7a589ccf105e5ad
Héctor Reyes López <hector.reyes.lopez.17@gmail.com>
2024-05-07 13:03:45 +0200

    Update generators.ts

--- src/components/data-table/generators.ts
+++ src/components/data-table/generators.ts
@@ -38,9 +38,10 @@ const generateActionColumns = (): any => {
     hozAlign: 'center',
     resizable: false,
     frozen: true,
-    maxWidth: 100,
-    minWidth: 100,
-    width: 100,
+    // maxWidth: 100,
+    // minWidth: 100,
+    // width: 100,
+    widthGrow: 5,
     formatter: cell => {
       const _cellNumber = cell.getRow().getData().index;
       const _container = document.createElement('div');
